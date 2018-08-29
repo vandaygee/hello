143b0ad4df1dadbee81c470fc22c5257715f8cdb
user <user@DESKTOP-UOC7GUS>
2018-08-29 15:51:31 +0100

    new line

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -17,6 +17,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("we're just testing");
+        System.out.println("New line");
     }
     
 }
